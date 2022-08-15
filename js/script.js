89471939578abc8b7f358fa54a6bd1127be55136
goldipl <mgodlewski92@gmail.com>
2022-08-15 15:48:34 +0200

    styling background image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let left_position = 40;
 let bottom_position = 4;
+let background_position = 0;
 
 /**
  * When the user presses a key, the cyborg moves left or right.
@@ -7,6 +8,7 @@ let bottom_position = 4;
  */
 const move = (e) => {
     const cyborg = document.querySelector(".cyborg-image");
+    const background = document.querySelector(".background-image");
     switch (e.keyCode) {
         case 37:
         // Move left
@@ -16,10 +18,12 @@ const move = (e) => {
         case 39:
         // Move right
         left_position += 5;
+        background_position -= 5;
         break;
     }
     cyborg.style.left = `${left_position}px`;
     cyborg.style.bottom = `${bottom_position}px`;
+    background.style.left = `${background_position}px`;
 }
 
 document.addEventListener("keydown", move);
