b6e718c3173bb59283b8e9d3b1e0e92cc90b8695
goldipl <mgodlewski92@gmail.com>
2022-08-17 21:53:00 +0200

    change logic to jump

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-/* This is the code that is responsible for the movement of the cyborg. */
-let bottom_position = 0;
-let background_position = 0;
-
 const cyborg = document.querySelector(".cyborg-image");
 const background = document.querySelector(".background-image");
+
+/* Creating a monster and placing it on the screen. */
+const monster = document.createElement('img');
+monster.src = './img/monster.svg';
+monster.width = 80;
+monster.height = 80;
+monster.classList.add('monster');
+const container = document.querySelector('.game-screen .container .game__section').appendChild(monster);
 
 /**
  * When the user presses the up arrow key, the cyborg's bottom position is set to 100px.
@@ -13,23 +17,16 @@ const move = (e) => {
     switch (e.keyCode) {
         case 32:
         // Jump - spacebar
-        bottom_position = 100;
+        cyborg.classList.add('jump');
+        setTimeout(() => {
+            cyborg.classList.remove('jump')
+        }, 500);
         break;
     }
-    
-    cyborg.style.bottom = `${bottom_position}px`;
 };
 
 /* Listening for a keydown event and then calling the move function. */
 document.addEventListener("keydown", move);
-
-/* Creating a monster and placing it on the screen. */
-const monster = document.createElement('img');
-monster.src = './img/monster.svg';
-monster.width = 80;
-monster.height = 80;
-monster.classList.add('monster');
-const container = document.querySelector('.game-screen .container .game__section').appendChild(monster);
 
 checkCollision = () => {
     if (monster.offsetLeft === cyborg.offsetLeft)  {
