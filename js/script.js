799107a29469f79ba87a4398493324e0a6bdfeaa
goldipl <mgodlewski92@gmail.com>
2022-08-17 21:42:14 +0200

    change logic, change e.keyCode to 32 for jumping

--- js/script.js
+++ js/script.js
@@ -1,42 +1,29 @@
-/* This is the code that moves the cyborg. */
-let left_position = 40;
+/* This is the code that is responsible for the movement of the cyborg. */
 let bottom_position = 0;
 let background_position = 0;
 
+const cyborg = document.querySelector(".cyborg-image");
+const background = document.querySelector(".background-image");
+
 /**
- * The function moves the cyborg image left and right, and moves the background image left.
- * @param e - The event object.
+ * When the user presses the up arrow key, the cyborg's bottom position is set to 100px.
+ * @param e - The event object
  */
 const move = (e) => {
-    const cyborg = document.querySelector(".cyborg-image");
-    const background = document.querySelector(".background-image");
     switch (e.keyCode) {
-        case 37:
-        // Move left
-        left_position -= 5;
-        break;
-
-        case 39:
-        // Move right
-        left_position += 2;
-        if (cyborg.style.left === `180px`) {
-            left_position = 180;
-        }
-        background_position -= 5;
+        case 32:
+        // Jump - spacebar
+        bottom_position = 100;
         break;
     }
     
-    cyborg.style.left = `${left_position}px`;
     cyborg.style.bottom = `${bottom_position}px`;
-    background.style.left = `${background_position}px`;
-}
+};
 
 /* Listening for a keydown event and then calling the move function. */
 document.addEventListener("keydown", move);
 
 /* Creating a monster and placing it on the screen. */
-let monster_position = 600;
-
 const monster = document.createElement('img');
 monster.src = './img/monster.svg';
 monster.width = 80;
@@ -44,4 +31,10 @@ monster.height = 80;
 monster.classList.add('monster');
 const container = document.querySelector('.game-screen .container .game__section').appendChild(monster);
 
-monster.style.left = `${monster_position}px`;
+checkCollision = () => {
+    if (monster.offsetLeft === cyborg.offsetLeft)  {
+        console.log("Live -1");
+    }
+};
+
+setInterval(checkCollision, 10);
