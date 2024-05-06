1d68511908379744e6286b7f666ed23641b7f839
goldipl <mgodlewski92@gmail.com>
2024-05-06 11:41:31 +0200

    refactor code, clean architecture, add img folders, clean projects code

--- js/characters/monster.js
+++ js/characters/monster.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.game-screen .container .game__section
 
 export const createMonster = () => {
     const monster = document.createElement('img');
-    monster.src = './img/monster.svg';
+    monster.src = './img/characters/monster.svg';
     monster.width = 80;
     monster.height = 80;
     monster.classList.add('monster');
