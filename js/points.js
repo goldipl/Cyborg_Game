02b0900313520d9372f0778f073197f356292b5a
goldipl <mgodlewski92@gmail.com>
2024-05-08 10:47:03 +0200

    refactor points script

--- js/points.js
+++ js/points.js
@@ -1,8 +1,12 @@
-export const addPoints = (pointsQuantity) => {
+const addPoints = (pointsQuantity) => {
     const pointsContainer = document.querySelector('.points-container');
     let currentPoints = parseInt(pointsContainer.innerText);
 
     currentPoints += pointsQuantity;
     const formattedPoints = currentPoints.toString().padStart(6, '0');
     pointsContainer.innerText = formattedPoints;
+    
+    return formattedPoints;
 };
+
+export { addPoints };
